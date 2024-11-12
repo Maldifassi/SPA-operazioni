4b2f780e1b4d27fda298971d272b2dcb02f4e3f3
Riccardo Maldifassi <maldifassi.riccardo@iisgalvanimi.edu.it>
2024-11-12 11:49:51 +0000

    server API post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 #server web - codice per restituire la homepage
 @app.route('/')
@@ -8,12 +8,24 @@ def homepage():
 #server API
 @app.route('/calcola', methods = ['POST'])
 def calcola():
-    dati = request.get_json
-    return
-
-
-
-
-
+    #servono per prendere le info che arrivano dal front-end
+    json_data = request.get_json()
+    if json_data:
+        num1 =  json_data.get('num1')
+        num2 = json_data.get('num2')
+        operazione = json_data.get('operazione')
+        #elaborazione delle info
+        if operazione == 'addizione':
+            risultato = num1 + num2
+        elif operazione == 'sottrazione':
+            risultato = num1 - num2
+        elif operazione == 'moltiplicazione':
+            risultato = num1 * num2
+        elif operazione == 'divisione':
+            risultato = num1 // num2
+            #restituire i risultati al front-end
+        return jsonify(risultato = risultato)
+    else:
+        return jsonify(risultato = 'mancano i dati')
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 3245, debug = True) #0.0.0.0 accetta connessioni da qualunque indirizzo ip
